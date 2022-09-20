d4a4a8be577a9917c05366b67b1adc942e9c7dc8
Alex-andre-a <ptitmitch44@gmail.com>
2022-09-20 11:02:12 +0200

    En cours !

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+    $email = "alexandre.arfini@gmail.com";
+    $name = "Alexandre";
+    $body = "Bonjour j'espèque que vous avez bien reçu mon message :) <br><br><a href='https://github.com/Alex-andre-a/Defi-Ouivalo'>Lien GH</a>";
+    $subject = "CLE-TEST-IOT";
+
+    $headers = array(
+        'Authorization: Bearer YOUR-TOKEN',
+        'Content-Type: application/json'
+    );
+
+    $data = array(
+        "personalizations" => array(
+            array(
+                "to" => array(
+                    array(
+                        "email" => $email,
+                        "name" => $name
+                    )
+                )
+            )
+        ),
+        "from" => array(
+            "email" => "candidat.e@ouivalo.fr"
+        ),
+        "subject" => $subject,
+        "content" => array(
+            array(
+                "type" => "text/html",
+                "value" => $body
+            )
+        )
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    echo $response;
+?>
